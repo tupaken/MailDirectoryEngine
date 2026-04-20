b90c78c1c9df0eaf5f0d0ff16e0495752633ee29
Praktikant <praktikant@pbh.pbhanke.de>
2026-04-20 14:15:20 +0200

    feat(mail-storage-sevice): Add storage service store endpoint

--- MailStorageService/src/Program.cs
+++ MailStorageService/src/Program.cs
@@ -1,11 +1,23 @@
 using DotNetEnv;
+using MailStorageService.Storage;
+using Sprache;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 Env.Load(Path.Combine(builder.Environment.ContentRootPath, ".env"));
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/store", (StoreRequest request) =>
+{
+    var storageEngine = new StorageEngine();
+    var success = storageEngine.Store( request.SourcePath,request.Number);
+
+    return success
+        ? Results.Ok(new {message = "200"})
+        : Results.BadRequest(new {message = "400"});
+} );
 
 
 app.Run();
+
+internal sealed record StoreRequest(string SourcePath, string Number);
